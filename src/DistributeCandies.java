58fc25b7a99ec0b6a5804e0a56320c4524930a8a
vishal garg <vishal.v.garg@oracle.com>
2017-10-15 03:15:46 -0700

    Add Distribution candies program

--- src/DistributeCandies.java
+++ src/DistributeCandies.java
@@ -1,6 +1,5 @@
 import java.util.HashSet;
 import java.util.Set;
-
 /*Given an integer array with even length, where different numbers in this array represent
 different kinds of candies. Each number means one candy of the corresponding kind. 
 You need to distribute these candies equally in number to brother and sister. 
