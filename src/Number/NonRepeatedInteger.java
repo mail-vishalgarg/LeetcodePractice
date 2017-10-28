c7d23d8ce02a1c40a2409ffcfc4c559ab223503e
vishal garg <vishal.v.garg@oracle.com>
2017-10-28 15:42:07 -0700

    Leetcode practice code

--- src/Number/NonRepeatedInteger.java
+++ src/Number/NonRepeatedInteger.java
@@ -23,14 +23,12 @@ public class NonRepeatedInteger {
 	
 	public static void nonRepeatedUniqInt(int[] num){
 		int lui = 0;
-		for (int i=0; i<num.length -1; i++){
-			if (num[i] == num[i+1]){
+		for (int i=1; i<num.length; i++){
+			if (num[i] == num[i-1]){
 				lui = i+1;
-				num[lui] = num[i+1];
-				
 			} 
 		}
-		for(int i=0; i<=lui; i++){
+		for(int i=lui; i<=num.length -1; i++){
 			System.out.println(num[i]);
 		}
 		
@@ -38,7 +36,7 @@ public class NonRepeatedInteger {
 	
 	public static void main(String[] args) {
 		int[] num = {1,1,2,2,3,4};
-		nonRepeatedInt(num);
+		//nonRepeatedInt(num);
 		nonRepeatedUniqInt(num);
 	}
 }
