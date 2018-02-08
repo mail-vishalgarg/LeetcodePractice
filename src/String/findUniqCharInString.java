3c48f1613d93ab4651d1c74965a7fb252349a09c
vishal garg <vishal.v.garg@oracle.com>
2018-02-08 14:36:42 -0800

    new code

--- src/String/findUniqCharInString.java
+++ src/String/findUniqCharInString.java
@@ -6,7 +6,10 @@ public class findUniqCharInString {
 		System.out.println((int)'a');
 		int[] freq = new int[26];
 		for(int i=0; i< s.length()-1; i++){
-			freq[(int)s.charAt(i) -'a']++;
+			System.out.println("char value :" + (int)s.charAt(i));
+			int charValueInInt = (int)s.charAt(i) -'a';
+			System.out.println("Char value in integer :" + charValueInInt);
+			freq[charValueInInt]++;
 		}
 		for(int y=0; y<s.length()-1;y++){
 			if ((freq[s.charAt(y) - 'a']) == 1){
